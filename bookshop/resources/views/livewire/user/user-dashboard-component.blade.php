f438ea6ffa3390eced27077634661f1bc2f160d8
sana-kabbani <57013014+sana-kabbani@users.noreply.github.com>
2023-04-09 20:33:16 +0300

    first

---
+++ bookshop/resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,4 @@
+<div>
+<h1> User Dashboard
+</h1>
+</div>
---
+++ bookshop/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+Route::get('/', HomeComponent::class)->name('home.index');
+Route::get('/', ShopComponent::class)->name('shop');
+Route::get('/', CartComponent::class)->name('shop.cart');
+Route::get('/', CheckoutComponent::class)->name('shop.checkout');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
